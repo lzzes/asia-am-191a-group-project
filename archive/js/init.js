5644bb577ccfbb2eebd94c402ad23cb435a4ad02
snguyen1237 <snguyen1237@ucla.edu>
2026-07-25 10:22:53 -0700

    update homepage and archive

--- archive/js/init.js
+++ archive/js/init.js
@@ -1,74 +1,163 @@
 // Declare global variables
 
 
-const mapOptions = {
+let mapOptions = {
     "zoom": 15,
     "center" : [-118.442199,34.072225],
 }
 
+let dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQl_X4nqI_LmgvWzm_OMkH-uAkYyf3T8M5wxARgbjR30lbC7cfztoJ6gEvmC57nV7sks846j5uEtaS_/pub?gid=592139015&single=true&output=csv"
+
 // Initialize the map
 const map = new maplibregl.Map({
     container: 'map',
-    style: 'https://api.maptiler.com/maps/streets-v2-light/style.json?key=wsyYBQjqRwKnNsZrtci1',
+    style: 'https://api.maptiler.com/maps/streets-v2-light/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
     center: mapOptions.center,
     zoom: mapOptions.zoom
 });
 
-function addMarker(lat,lng,title,message){
-    let popup_message = `<h2>${title}</h2> <h3>${message}</h3>`
-    new maplibregl.Marker()
-        .setLngLat([lng, lat])
-        .setPopup(new maplibregl.Popup()
-            .setHTML(popup_message))
-        .addTo(map)
-    createButtons(lat,lng,title);
-    return message
-}
-
-
-let results;
-map.on('load',function(){
-    console.log("girl. it's loaded.")
-    fetch('untitled.geojson')
-         .then(data =>{
-            return data.json();
-    })
-         .then(data =>{
-            console.log(data)
-            data.features.forEach(processData);
-    })
+map.on('load', function() {
+    // Use PapaParse to fetch and parse the CSV data from a Google Forms spreadsheet URL
+    Papa.parse(dataUrl, {
+        download: true, // Tells PapaParse to fetch the CSV data from the URL
+        header: true, // Assumes the first row of your CSV are column headers
+        complete: (results) => {
+            processData(results.data);
+        }
+    });
 });
 
-function processData(result){
-    console.log(result)
-    let longitude = result.geometry.coordinates[0]
-    let latitude = result.geometry.coordinates[1]
-    let title = result.properties.place
-    let color = result.properties.color
-    let description = result.properties.description
-    addMarker(latitude,longitude,title,description)
+
+function processData(results){
+    let IDarray = new Array(30);
+
+    let ID, lng, lat, location, action, date, repression, memory;
+    results.forEach(feature => {
+        console.log(feature)
+        ID = feature.ID;
+        lng = feature.lng;
+        lat = feature.lat;
+        location = feature["Mapping Location"];
+        action = feature.Name;
+        date = feature.Date;
+        repression = feature["Did the university engage or respond to this action in any way? "];
+        memory = feature["What was the most memorable part of the action? "];
+
+        //if testimonial exists AND the event has not already been added:
+        if (memory != false && IDarray.includes(ID) == false){
+            IDarray.push(ID);
+            addTestimonial(lng,lat,ID,location,action,date,repression,memory);
+        }
+        //else if testimonial exists for an already-added event:
+        else if (memory != false){
+            repeatTestimonial(repression,memory);
+        }
+        //else, the action has no testimony
+        else {
+            addAction(lat,lng,location,action,date);
+        }
+    });
 }
 
+function addTestimonial(lng,lat,ID,location,action,date,repression,memory){
+    //stuff i need to define in order to add things to portfolio section
+    const testimoniesDef = document.getElementById("testimonies");
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("testimonyChunk");
 
-function createButtons(lat,lng,title){
-    const newButton = document.createElement("button");
-    newButton.id = "button"+title;
-    newButton.innerHTML = title;
+    // all of da button stuffs
+    const newButton = document.createElement("button"); 
+    newButton.id = "button"+action;
+    newButton.innerHTML = action;
     newButton.setAttribute("lat",lat);
     newButton.setAttribute("lng",lng);
     newButton.classList.add("button-17")
     newButton.addEventListener('click', function(){
         map.flyTo({
             center: [lng,lat],
-            zoom: 18
-
+            zoom: 19
         })
-    })
-    document.getElementById("contents").appendChild(newButton);
+    });
+   
+    // make a new header w the location + date
+    const heading3 = document.createElement("h3");
+    heading3.textContent = location + ", " + date; 
+
+    const heading4 = document.createElement("h4");
+    heading4.textContent = "One person testifies:"
+
+    // insert paragraph of testimonial text
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "Testified repression: " + repression + " Memories associated: " + memory; 
+
+    // i don;t really know the guide online told me to do this. something about the divisions
+    wrapper.appendChild(newButton);
+    wrapper.appendChild(heading3);
+    wrapper.appendChild(heading4);
+    wrapper.appendChild(paragraph);
+    testimoniesDef.appendChild(wrapper);
+
+    addMarker(lng,lat,action, date);
 }
 
+function repeatTestimonial(repression,memory){
+    const testimoniesDef = document.getElementById("testimonies");
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("testimonyChunk");
 
+        // make a new header w the location + date
+    const heading = document.createElement("h4");
+    heading.textContent = "Another testifies:";
 
+    // insert paragraph of testimonial text
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "Testified repression: " + repression + " Memories associated: " + memory; 
 
-//.forEach => 
-//to add to arrays use arrayName.push(thing)
+    // i don;t really know the guide online told me to do this. something about the divisions
+    wrapper.appendChild(heading);
+    wrapper.appendChild(paragraph);
+    testimoniesDef.appendChild(wrapper);
+}
+
+function addAction(lat,lng,location,action,date){
+ //stuff i need to define in order to add things to portfolio section
+    const actionsDef = document.getElementById("actions");
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("testimonyChunk");
+
+    // all of da button stuffs
+    const newButton = document.createElement("button"); 
+    newButton.id = "button"+action;
+    newButton.innerHTML = action;
+    newButton.setAttribute("lat",lat);
+    newButton.setAttribute("lng",lng);
+    newButton.classList.add("button-17")
+    newButton.addEventListener('click', function(){
+        map.flyTo({
+            center: [lng,lat],
+            zoom: 19
+        })
+    });
+   
+    // make a new header w the location + date
+    const heading = document.createElement("h4");
+    heading.textContent = location + ", " + date;  
+
+    // i don;t really know the guide online told me to do this. something about the divisions
+    wrapper.appendChild(newButton);
+    wrapper.appendChild(heading);
+    actionsDef.appendChild(wrapper);
+
+    addMarker(lng,lat,action, date);
+}
+
+//if we wanted to we could pass in the action type here. would have to go back a ways though
+// because we only arrive here by passing through at least one other function
+function addMarker(lng,lat,action, date){
+    let popup_message = `<h2>${action}</h2> <h3>${date}</h3>`
+    new maplibregl.Marker()
+        .setLngLat([lng, lat])
+        .setPopup(new maplibregl.Popup().setHTML(popup_message))
+        .addTo(map)
+    return action;
+}
--- home/js/init.js
+++ home/js/init.js
@@ -1,29 +1,39 @@
-// Initialize the map
-const map = new maplibregl.Map({
-    container: 'maps', // container ID
+// Initialize the maps
+
+const map1 = new maplibregl.Map({
+    container: 'map1', // container ID
     style: 'https://api.maptiler.com/maps/streets-v2-light/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
-    center: [-118.44014, 34.07356], // Starting position [lng, lat]
-    zoom: 16 // Starting zoom level
+    center: [-118.44450, 34.07009], // Starting position [lng, lat] 34.07009° N, 118.44450° W
+    zoom: 13 // Starting zoom level
 });
 
+const map2 = new maplibregl.Map({
+    container: 'map2', // container ID
+    style: 'https://api.maptiler.com/maps/streets-v2-light/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
+    center: [-118.44450, 34.07009], // Starting position [lng, lat]
+    zoom: 13 // Starting zoom level
+});
 
+const map3 = new maplibregl.Map({
+    container: 'map3', // container ID
+    style: 'https://api.maptiler.com/maps/streets-v2-light/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
+    center: [-118.44450, 34.07009], // Starting position [lng, lat]
+    zoom: 13 // Starting zoom level
+});
 
-
-
-function addMarker(lat,lng,location,message,person, date){
+function addMarker(lat,lng,location,message,person,date, portfolio, map){
     let popup_message = `<h2>${location}</h2> <h3>${person}</h3>`
     new maplibregl.Marker()
         .setLngLat([lng, lat])
         .setPopup(new maplibregl.Popup().setHTML(popup_message))
         .addTo(map)
-    createButtons(lat,lng,message,person,location,date);
+    createButtons(lat,lng,message,person,location,date, portfolio, map);
     return message
 }
 
-// creating not just the button but the whole testimonial chunk too
- function createButtons(lat,lng,message,person,location,date){
+function createButtons(lat,lng,message,person,location,date, portfolio, map){
     //stuff i need to define in order to add things to portfolio section
-    const portfolio = document.getElementById("portfolio");
+    const portfolioDef = document.getElementById(portfolio);
     const wrapper = document.createElement("div");
     wrapper.classList.add("testimonyChunk");
 
@@ -45,7 +55,7 @@ function addMarker(lat,lng,location,message,person, date){
     const heading = document.createElement("h4");
     heading.textContent = location + ", " + date; 
 
-    // insert the testimonial text
+    // insert paragraph of testimonial text
     const paragraph = document.createElement("p");
     paragraph.textContent = message; 
 
@@ -53,15 +63,460 @@ function addMarker(lat,lng,location,message,person, date){
     wrapper.appendChild(newButton);
     wrapper.appendChild(heading);
     wrapper.appendChild(paragraph);
-    portfolio.appendChild(wrapper);
+    portfolioDef.appendChild(wrapper);
 } 
 
-
 function init(){
-    addMarker(34.07308, -118.43896, "Shapiro Courtyard", "Here i experienced the world.", "Testimonial 1", "Spring 2024")
-    addMarker(34.07489, -118.44007, "Franklin D. Murphy Sculpture Garden", "I read things here", "Testimonial 2", "Fall 2025")
-    addMarker(34.07391, -118.44209, "Rolfe Court", "I planned things here", "Testimonial 3", "Winter 2024")
+    addMarker(
+        34.07308, -118.43896, 
+        "Shapiro Courtyard", 
+        "Here i experienced the world. More text more text more text paragraph paragraph paragraph More text more text more text paragraph paragraph paragraph More text more text more text paragraph paragraph paragraph", 
+        "Testimonial 1", 
+        "Spring 2024", 
+        "portfolio1", map1)
+    addMarker(34.07489, -118.44007, 
+        "Franklin D. Murphy Sculpture Garden", 
+        "2 Here i experienced the world. More text more text more text paragraph paragraph paragraph More text more text more text paragraph paragraph paragraph More text more text more text paragraph paragraph paragraph", 
+        "Testimonial 2", 
+        "Fall 2025", 
+        "portfolio2", map2)
+    addMarker(34.07391, -118.44209, 
+        "Rolfe Court", 
+        "3Here i experienced the world. More text more text more text paragraph paragraph paragraph More text more text more text paragraph paragraph paragraph More text more text more text paragraph paragraph paragraph", 
+        "Testimonial 3", 
+        "Winter 2024", 
+        "portfolio3", map3)
 };
 
 init();
+
+// polygon stuff
+map1.on('load', () => {
+    map1.addSource('ucla', {
+        'type': 'geojson',
+        'data': {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [
+                    [
+                        [
+                            -118.4449953,
+                            34.0687278
+                        ],
+                        [
+                            -118.4451171,
+                            34.0670702
+                        ],
+                        [
+                            -118.4429265,
+                            34.0669664
+                        ],
+                        [
+                            -118.4417628,
+                            34.0670183
+                        ],
+                        [
+                            -118.4417674,
+                            34.0651578
+                        ],
+                        [
+                            -118.4422637,
+                            34.0639645
+                        ],
+                        [
+                            -118.4412745,
+                            34.0640533
+                        ],
+                        [
+                            -118.4402276,
+                            34.0656406
+                        ],
+                        [
+                            -118.4391636,
+                            34.0671427
+                        ],
+                        [
+                            -118.4390267,
+                            34.0696695
+                        ],
+                        [
+                            -118.4383515,
+                            34.0711001
+                        ],
+                        [
+                            -118.4372642,
+                            34.0721928
+                        ],
+                        [
+                            -118.437929,
+                            34.0741901
+                        ],
+                        [
+                            -118.4387703,
+                            34.0762808
+                        ],
+                        [
+                            -118.4391432,
+                            34.077926
+                        ],
+                        [
+                            -118.439614,
+                            34.0781434
+                        ],
+                        [
+                            -118.4407835,
+                            34.0774213
+                        ],
+                        [
+                            -118.4424331,
+                            34.0773854
+                        ],
+                        [
+                            -118.444017,
+                            34.0768488
+                        ],
+                        [
+                            -118.4447282,
+                            34.0756822
+                        ],
+                        [
+                            -118.4451273,
+                            34.073916
+                        ],
+                        [
+                            -118.4462326,
+                            34.0734727
+                        ],
+                        [
+                            -118.4486567,
+                            34.0733708
+                        ],
+                        [
+                            -118.4493959,
+                            34.0728972
+                        ],
+                        [
+                            -118.4495766,
+                            34.0711658
+                        ],
+                        [
+                            -118.4490328,
+                            34.0698628
+                        ],
+                        [
+                            -118.4484991,
+                            34.0687426
+                        ],
+                        [
+                            -118.4450229,
+                            34.0687426
+                        ],
+                        [
+                            -118.4449953,
+                            34.0687278
+                        ]
+                    ]
+                ]
+            }
+        }
+    });
+
+    map1.addLayer({
+        'id': 'ucla',
+        'type': 'fill',
+        'source': 'ucla',
+        'layout': {},
+        'paint': {
+            'fill-color': 'rgb(255, 200, 200)',
+            'fill-opacity': 0.7
+        }
+    });
+});
+
+map2.on('load', () => {
+    map2.addSource('ucla', {
+        'type': 'geojson',
+        'data': {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [
+                    [
+                        [
+                            -118.4449953,
+                            34.0687278
+                        ],
+                        [
+                            -118.4451171,
+                            34.0670702
+                        ],
+                        [
+                            -118.4429265,
+                            34.0669664
+                        ],
+                        [
+                            -118.4417628,
+                            34.0670183
+                        ],
+                        [
+                            -118.4417674,
+                            34.0651578
+                        ],
+                        [
+                            -118.4422637,
+                            34.0639645
+                        ],
+                        [
+                            -118.4412745,
+                            34.0640533
+                        ],
+                        [
+                            -118.4402276,
+                            34.0656406
+                        ],
+                        [
+                            -118.4391636,
+                            34.0671427
+                        ],
+                        [
+                            -118.4390267,
+                            34.0696695
+                        ],
+                        [
+                            -118.4383515,
+                            34.0711001
+                        ],
+                        [
+                            -118.4372642,
+                            34.0721928
+                        ],
+                        [
+                            -118.437929,
+                            34.0741901
+                        ],
+                        [
+                            -118.4387703,
+                            34.0762808
+                        ],
+                        [
+                            -118.4391432,
+                            34.077926
+                        ],
+                        [
+                            -118.439614,
+                            34.0781434
+                        ],
+                        [
+                            -118.4407835,
+                            34.0774213
+                        ],
+                        [
+                            -118.4424331,
+                            34.0773854
+                        ],
+                        [
+                            -118.444017,
+                            34.0768488
+                        ],
+                        [
+                            -118.4447282,
+                            34.0756822
+                        ],
+                        [
+                            -118.4451273,
+                            34.073916
+                        ],
+                        [
+                            -118.4462326,
+                            34.0734727
+                        ],
+                        [
+                            -118.4486567,
+                            34.0733708
+                        ],
+                        [
+                            -118.4493959,
+                            34.0728972
+                        ],
+                        [
+                            -118.4495766,
+                            34.0711658
+                        ],
+                        [
+                            -118.4490328,
+                            34.0698628
+                        ],
+                        [
+                            -118.4484991,
+                            34.0687426
+                        ],
+                        [
+                            -118.4450229,
+                            34.0687426
+                        ],
+                        [
+                            -118.4449953,
+                            34.0687278
+                        ]
+                    ]
+                ]
+            }
+        }
+    });
+
+    map2.addLayer({
+        'id': 'ucla',
+        'type': 'fill',
+        'source': 'ucla',
+        'layout': {},
+        'paint': {
+            'fill-color': 'rgb(255, 200, 200)',
+            'fill-opacity': 0.7
+        }
+    });
+});
+
+map3.on('load', () => {
+    map3.addSource('ucla', {
+        'type': 'geojson',
+        'data': {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [
+                    [
+                        [
+                            -118.4449953,
+                            34.0687278
+                        ],
+                        [
+                            -118.4451171,
+                            34.0670702
+                        ],
+                        [
+                            -118.4429265,
+                            34.0669664
+                        ],
+                        [
+                            -118.4417628,
+                            34.0670183
+                        ],
+                        [
+                            -118.4417674,
+                            34.0651578
+                        ],
+                        [
+                            -118.4422637,
+                            34.0639645
+                        ],
+                        [
+                            -118.4412745,
+                            34.0640533
+                        ],
+                        [
+                            -118.4402276,
+                            34.0656406
+                        ],
+                        [
+                            -118.4391636,
+                            34.0671427
+                        ],
+                        [
+                            -118.4390267,
+                            34.0696695
+                        ],
+                        [
+                            -118.4383515,
+                            34.0711001
+                        ],
+                        [
+                            -118.4372642,
+                            34.0721928
+                        ],
+                        [
+                            -118.437929,
+                            34.0741901
+                        ],
+                        [
+                            -118.4387703,
+                            34.0762808
+                        ],
+                        [
+                            -118.4391432,
+                            34.077926
+                        ],
+                        [
+                            -118.439614,
+                            34.0781434
+                        ],
+                        [
+                            -118.4407835,
+                            34.0774213
+                        ],
+                        [
+                            -118.4424331,
+                            34.0773854
+                        ],
+                        [
+                            -118.444017,
+                            34.0768488
+                        ],
+                        [
+                            -118.4447282,
+                            34.0756822
+                        ],
+                        [
+                            -118.4451273,
+                            34.073916
+                        ],
+                        [
+                            -118.4462326,
+                            34.0734727
+                        ],
+                        [
+                            -118.4486567,
+                            34.0733708
+                        ],
+                        [
+                            -118.4493959,
+                            34.0728972
+                        ],
+                        [
+                            -118.4495766,
+                            34.0711658
+                        ],
+                        [
+                            -118.4490328,
+                            34.0698628
+                        ],
+                        [
+                            -118.4484991,
+                            34.0687426
+                        ],
+                        [
+                            -118.4450229,
+                            34.0687426
+                        ],
+                        [
+                            -118.4449953,
+                            34.0687278
+                        ]
+                    ]
+                ]
+            }
+        }
+    });
+
+    map3.addLayer({
+        'id': 'ucla',
+        'type': 'fill',
+        'source': 'ucla',
+        'layout': {},
+        'paint': {
+            'fill-color': 'rgb(255, 200, 200)',
+            'fill-opacity': 0.7
+        }
+    });
+});
 
