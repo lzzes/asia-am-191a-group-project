d684060bbb80402c3ff210afc055a2a8b38fceca
e-sang <sevan1751@gmail.com>
2026-07-27 14:44:00 -0400

    archive-popup

--- archive/js/init.js
+++ archive/js/init.js
@@ -209,3 +209,73 @@ function addMarker(lng,lat,action, date, form){
         .addTo(map)
     return action;
 }
+
+/* modal situation */
+
+// Get the modal
+var modal2 = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
+window.onload = function() {
+    modal2.style.display = "block";
+  };
+  
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
+
+window.addEventListener("resize", scroller.resize);
+
+function closeModal(e){
+    let parentDiv = e.target.parentElement.id
+    console.log(parentDiv)
+    /*if(parentDiv != "surveyBtn"){
+        modal.style.display = "none";
+    }
+    if(parentDiv == "menuButtons"){
+        modal.style.display = "none";
+    }*/
+}
+document.body.addEventListener('click', function(e) {
+    closeModal(e)
+        // your code
+    // console.log(e.target.id)
+});
+// document.iframe.addEventListener('click', closeModal, true); 
+
+/* // Get the modal
+var modal = document.getElementById("surveyModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("surveyBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "grid";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} */
